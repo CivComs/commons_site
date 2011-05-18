e186fcbd9663e17c4703ed4e1a768e993e611cb4
jerseycoder <michelle@codeforamerica.org>
2011-05-18 16:01:34 -0700

    Made simple index view to show names of apps

--- commons/urls.py
+++ commons/urls.py
@@ -9,7 +9,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'commons.views.home', name='home'),
     # url(r'^commons/', include('commons.foo.urls')),
     (r'^$', "commons.views.index"),
-
+    (r'^apps/(?P<app_id>\d+)/$', 'commons.views.detail'),
+   # (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+   # (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+   # (r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
