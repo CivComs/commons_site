cc0a2586ee151b8b49f6d8ca19fa3ae5130855dc
Michael Bernstein <michael@fandomhome.com>
2011-05-24 17:32:39 -0700

    Tying together the deployments, apps, and jurisdictions.

--- commons_project/commons_core/views.py
+++ commons_project/commons_core/views.py
@@ -39,16 +39,16 @@ def depdetail(request, dep_id):
 def detail(request, app_id):
     n = App.objects.get(pk=app_id)
     q = request.GET.get('q', n.name)
-    jxs = Deployment.objects.filter(app=app_id)
     results = GoogleSearch.fetch(q)
-    return render_to_response("detail.html", {'results':results,'app':n, 'jurisdictions':jxs}, context_instance=RequestContext(request))
+    return render_to_response("detail.html", {'results':results,'app':n,},
+                              context_instance=RequestContext(request))
 
 # Jurisdictions
 
 def j_index(request):
     j = Jurisdiction.objects.all()
     return render_to_response('j_index.html', {'jurisdiction_list': j},
-                             context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
 
 def j_detail(request, j_id):
     j = Jurisdiction.objects.get(pk=j_id)
